e042551cbae0bd495d71da8bc867f9c62f1212d4
Cameron Fletcher <cameron.fletcher@cameronfletcher.com>
2013-05-21 20:51:08 +0200

    Added test projects for different build types.

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿// <copyright file="CommonAssemblyInfo.cs" company="Guardian contributors">
 //  Copyright (c) Guardian contributors. All rights reserved.
 // </copyright>
-// <summary>I am Guardian. Mostly of null values.</summary>
+// <summary>Guardian. Mostly of null values.</summary>
 
 using System.Diagnostics.CodeAnalysis;
 using System.Reflection;
---
+++ src/tests/Guardian.Net35.Debug.Tests/InvalidGuardTests.cs
@@ -0,0 +1,200 @@
+﻿// <copyright file="InvalidGuardTests.cs" company="Guardian contributors">
+//  Copyright (c) Guardian contributors. All rights reserved.
+// </copyright>
+
+namespace Guardian.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using FluentAssertions;
+    using Xunit;
+
+    public class InvalidGuardTests
+    {
+        [Fact]
+        public void DefaultExpression()
+        {
+            // act
+            var exception = Record.Exception(() => Guard.Against.Null(() => default(object)));
+
+            // assert
+            exception.ShouldBeStrictArgumentNullException();
+        }
+
+        [Fact]
+        public void BlockExpression()
+        {
+            // act
+            var exception = Record.Exception(() => Guard.Against.Null(() => { return (object)null; }));
+
+            // assert
+            exception.ShouldBeStrictArgumentNullException();
+        }
+
+        [Fact]
+        public void ConditionalExpression()
+        {
+            // act
+#pragma warning disable 429
+            var exception = Record.Exception(() => Guard.Against.Null(() => 10 > 2 ? null : string.Empty));
+#pragma warning restore 429
+
+            // assert
+            exception.ShouldBeStrictArgumentNullException();
+        }
+
+        [Fact]
+        public void ConstantExpression()
+        {
+            // act
+            var exception = Record.Exception(() => Guard.Against.Null(() => (object)null));
+
+            // assert
+            exception.ShouldBeStrictArgumentNullException();
+        }
+
+        [Fact]
+        public void ObjectArray()
+        {
+            // arrange
+            var array = new object[] { new object(), null };
+
+            // act
+            var exception = Record.Exception(() => Guard.Against.Null(() => array[1]));
+
+            // assert
+            exception.ShouldBeStrictArgumentNullException();
+        }
+
+        [Fact]
+        public void ObjectList()
+        {
+            // arrange
+            var list = new List<object>(new object[] { new object(), null });
+
+            // act
+            var exception = Record.Exception(() => Guard.Against.Null(() => list[1]));
+
+            // assert
+            exception.ShouldBeStrictArgumentNullException();
+        }
+
+        [Fact]
+        public void ObjectDictionary()
+        {
+            // arrange
+            var dictionary = new Dictionary<string, string> { { "key", null } };
+
+            // act
+            var exception = Record.Exception(() => Guard.Against.Null(() => dictionary["key"]));
+
+            // assert
+            exception.ShouldBeStrictArgumentNullException();
+        }
+
+        [Fact]
+        public void ValueArray()
+        {
+            // arrange
+            var array = new int?[] { 1, null };
+
+            // act
+            var exception = Record.Exception(() => Guard.Against.Null(() => array[1]));
+
+            // assert
+            exception.ShouldBeStrictArgumentNullException();
+        }
+
+        [Fact]
+        public void ValueList()
+        {
+            // arrange
+            var list = new List<int?>(new int?[] { 1, null });
+
+            // act
+            var exception = Record.Exception(() => Guard.Against.Null(() => list[1]));
+
+            // assert
+            exception.ShouldBeStrictArgumentNullException();
+        }
+
+        [Fact]
+        public void ValueDictionary()
+        {
+            // arrange
+            var dictionary = new Dictionary<int?, string> { { 2, null } };
+
+            // act
+            var exception = Record.Exception(() => Guard.Against.Null(() => dictionary[2]));
+
+            // assert
+            exception.ShouldBeStrictArgumentNullException();
+        }
+
+        [Fact]
+        public void MethodTest()
+        {
+            // arrange
+            var thing = new Thing();
+
+            // act
+            var exception = Record.Exception(() => Guard.Against.Null(() => thing.Method()));
+
+            // assert
+            exception.ShouldBeStrictArgumentNullException();
+        }
+
+        [Fact]
+        public void NestedMethodTest()
+        {
+            // arrange
+            var thing = new Thing();
+
+            // act
+            var exception = Record.Exception(() => Guard.Against.Null(() => thing.NestedThing.Method()));
+
+            // assert
+            exception.ShouldBeStrictArgumentNullException();
+        }
+
+        [Fact]
+        public void ExtensionMethod()
+        {
+            // arrange
+            var array = new string[] { };
+
+            // act
+            var exception = Record.Exception(() => Guard.Against.Null(() => array.FirstOrDefault()));
+
+            // assert
+            exception.ShouldBeStrictArgumentNullException();
+        }
+
+        [Fact]
+        public void MethodWithArguments()
+        {
+            // arrange
+            var parameter = new Thing();
+
+            // act
+            var exception = Record.Exception(() => Guard.Against.Null(() => parameter.Method("argument")));
+
+            // assert
+            exception.ShouldBeStrictArgumentNullException();
+        }
+
+        [Fact]
+        public void NonNullExpression()
+        {
+            // arrange
+            var dictionary = new Dictionary<int?, string> { { 2, string.Empty } };
+
+            // act
+            var exception = Record.Exception(() => Guard.Against.Null(() => dictionary[2]));
+
+            // assert
+            exception.ShouldBeStrictNull();
+        }
+    }
+}
