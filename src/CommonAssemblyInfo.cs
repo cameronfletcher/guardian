2aceb561cc4737fd50badf8344c76cb0ac60814c
Cameron Fletcher <cameron.fletcher@cameronfletcher.com>
2013-05-29 22:34:30 +0200

    Added build script with Nuget package and publish.

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="CommonAssemblyInfo.cs" company="Guardian contributors">
+// <copyright file="CommonAssemblyInfo.cs" company="Guardian contributors">
 //  Copyright (c) Guardian contributors. All rights reserved.
 // </copyright>
 // <summary>Guardian. Mostly of null values.</summary>
@@ -13,8 +13,8 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyProduct("Guardian")]
 
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
-[assembly: AssemblyInformationalVersion("0.2.0-beta02")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: AssemblyInformationalVersion("0.0.0-alpha")]
 
 [assembly: SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly", Justification = "Using SemVer.")]
