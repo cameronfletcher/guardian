0ab2fc9e9ef89c09f343b87b2fc17caf2aa3761f
cameronfletcher <cameron.fletcher@cameronfletcher.com>
2013-06-04 20:15:26 +0200

    #2 Updated GuardianExtensions.

--- src/Guardian.Pcl/GuardExtensions.cs
+++ src/Guardian.Pcl/GuardExtensions.cs
@@ -22,9 +22,10 @@ internal static class GuardExtensions
     /// Guard against null or empty argument values.
     /// </summary>
     /// <typeparam name="T">The type of value to guard against.</typeparam>
-    /// <param name="guard">The Guard class.</param>
+    /// <param name="guard">The Guard clause.</param>
     /// <param name="expression">An expression returning the value to guard against.</param>
     [DebuggerStepThrough]
+    [Obsolete("This method is not fully supported in portable class libraries. Consider using Guard.Against.NullOrEmpty(value, parameterName) instead.")]
     [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = "May not be called.")]
     [SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "guard", Justification = "By design.")]
     public static void NullOrEmpty<T>(this Guard guard, Func<IEnumerable<T>> expression)
@@ -34,6 +35,27 @@ internal static class GuardExtensions
         if (!expression().Any())
         {
             throw new ArgumentException("Value cannot be empty.");
+        }
+    }
+
+    /// <summary>
+    /// Guard against null or empty argument values.
+    /// </summary>
+    /// <typeparam name="T">The type of value to guard against.</typeparam>
+    /// <param name="guard">The Guard clause.</param>
+    /// <param name="value">The value to guard against.</param>
+    /// <param name="parameterName">Name of the parameter.</param>
+    [DebuggerStepThrough]
+    [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = "May not be called.")]
+    [SuppressMessage("Microsoft.Usage", "CA1801:ReviewUnusedParameters", MessageId = "guard", Justification = "By design.")]
+    public static void NullOrEmpty<T>(this Guard guard, IEnumerable<T> value, string parameterName)
+    {
+        Guard.Against.Null(value, "value");
+        Guard.Against.Null(parameterName, "parameterName");
+
+        if (!value.Any())
+        {
+            throw new ArgumentException("Value cannot be empty.", parameterName);
         }
     }
 }
