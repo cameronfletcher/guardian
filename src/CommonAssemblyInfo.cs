aebeb21877720127b8e89e6f661cf3096c1c3910
Cameron Fletcher <cameron.fletcher@cameronfletcher.com>
2017-06-12 12:22:22 +0200

    Upgraded solution to work with vs2017

--- src/CommonAssemblyInfo.cs
+++ src/CommonAssemblyInfo.cs
@@ -13,8 +13,8 @@ using System.Reflection;
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyProduct("Guardian")]
 
-[assembly: AssemblyVersion("0.6.1.0")]
-[assembly: AssemblyFileVersion("0.6.1.0")]
-[assembly: AssemblyInformationalVersion("0.6.1-alpha")]
+[assembly: AssemblyVersion("0.8.0.0")]
+[assembly: AssemblyFileVersion("0.8.0.0")]
+[assembly: AssemblyInformationalVersion("0.8.0-alpha")]
 
 [assembly: SuppressMessage("Microsoft.Usage", "CA2243:AttributeStringLiteralsShouldParseCorrectly", Justification = "Using SemVer.")]
